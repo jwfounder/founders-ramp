2b17fc23687531a1883a0b9f2ebdfc71044b0886
jwfounder <justinweller.jw@gmail.com>
2026-09-11 11:51:32 -0400

    Load RB2B on every page that already runs hit.js

--- js/hit.js
+++ js/hit.js
@@ -1,4 +1,5 @@
-/* Founders Ramp inbound. Every visit. No third-party pixel. */
+/* Founders Ramp inbound. Every visit. RB2B identifies US visitors. */
+!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("Z6PVLHZZK96R");
 (function () {
   function send(extra) {
     var payload = {
